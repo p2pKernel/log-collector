b8f21c307917bd3d433e2b2a1be6c75474d813ed
lixl1 <lixl1@fun.tv>
2015-06-12 15:01:46 +0800

    update pre_process

--- LogCompress/pre_process_TSERVICEACCESQOS.sh
+++ LogCompress/pre_process_TSERVICEACCESQOS.sh
@@ -1,11 +1,3 @@
-pre_process=$1
+#! /bin/awk
 
-if [[ ${pre_process} && -s ${pre_process} ]]; then
-    echo 'ok'
-    source ${pre_process} 201506111105.log > out
-    if [[ $? -eq 0 ]]; then
-        mv out 201506111105.log
-    fi
-else
-    echo 'fail'
-fi
+awk -F'|' '{for(i=3; i<NF; i++) printf("%s|", $i); printf("\n");}' $1
